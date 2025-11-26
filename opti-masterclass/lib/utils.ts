56490f94af935f3a0e49fbde9c04475e92c78535
R-GOLUB-LAPTOP\r.golub <roman.golub@knowit.no>
2025-11-26 13:48:06 +0200

    add multilanguage support

--- opti-masterclass/lib/utils.ts
+++ opti-masterclass/lib/utils.ts
@@ -1,5 +1,17 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { ReadonlyURLSearchParams } from 'next/navigation';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const createUrl = (pathname: string, params: URLSearchParams | ReadonlyURLSearchParams) => {
+  const paramsString = params.toString();
+  const queryString = `${paramsString.length ? '?' : ''}${paramsString}`;
+
+  return `${pathname}${queryString}`;
+};
+
+export const leadingSlashUrlPath = (pathname: string) => {
+  return `${pathname.startsWith('/') ? '' : '/'}${pathname}`;
+};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
