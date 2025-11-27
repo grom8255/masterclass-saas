4206452b90ec448f6f5661a6ddb6ba84830cfacc
R-GOLUB-LAPTOP\r.golub <roman.golub@knowit.no>
2025-11-27 15:21:40 +0200

    fixing issues during deployment

--- opti-masterclass/components/block/hero-block.tsx
+++ opti-masterclass/components/block/hero-block.tsx
@@ -1,5 +1,6 @@
 // components\block\hero-block.tsx
 import { HeroBlock as HeroBlockProps } from '@/lib/optimizely/types/generated'
+import { cn } from '@/lib/utils'
 
 export default function HeroBlock({
   title,
@@ -9,23 +10,38 @@ export default function HeroBlock({
   decorationColorsSecondary = '#ffd285',
 }: HeroBlockProps) {
   return (
-    <section className="relative container mx-auto px-4 pt-20 pb-16">
-      <h1 data-epi-edit="title" className="mb-4 max-w-xl text-4xl font-bold md:text-6xl">{title}</h1>
-      {subtitle && (
-        <p data-epi-edit="subtitle" className="text-muted-foreground mb-8 max-w-xl text-xl">
-          {subtitle}
-        </p>
-      )}
+    <section
+      data-epi-edit="hero"
+      className="relative container mx-auto px-4 pt-20 pb-16"
+    >
+      <div className={cn('flex flex-col', { 'sm:pr-60': showDecoration })}>
+        <h1
+          className="mb-4 w-full max-w-xl text-4xl font-bold md:text-6xl"
+          data-epi-edit="title"
+        >
+          {title}
+        </h1>
+        {subtitle && (
+          <p
+            className="text-muted-foreground mb-8 max-w-xl text-xl"
+            data-epi-edit="subtitle"
+          >
+            {subtitle}
+          </p>
+        )}
+      </div>
       {showDecoration && (
         <div className="absolute top-10 right-20">
           <div className="relative h-48 w-48">
             <div
               className="absolute right-0 h-32 w-32 rounded-full"
-              style={{ backgroundColor: decorationColorsPrimary }}
+              style={{ backgroundColor: decorationColorsPrimary ?? '#009379' }}
             />
             <div
               className="absolute bottom-0 left-0 h-40 w-40 rounded-full"
-              style={{ backgroundColor: decorationColorsSecondary }}
+              style={{
+                backgroundColor: decorationColorsSecondary ?? '#ffd285',
+              }}
             />
           </div>
         </div>
--- opti-masterclass/next.config.ts
+++ opti-masterclass/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        hostname: 'res.cloudinary.com',
+      },
+    ],
     loader: 'custom',
     loaderFile: './lib/image/loader.ts',
   },
