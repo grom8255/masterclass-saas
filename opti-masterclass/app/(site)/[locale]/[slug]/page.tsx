8ad6913e2ea651975f672c68eb3a59564681e0d8
R-GOLUB-LAPTOP\r.golub <roman.golub@knowit.no>
2025-11-27 09:39:34 +0200

    apply changes for visual builder

--- opti-masterclass/app/(site)/[locale]/[slug]/page.tsx
+++ opti-masterclass/app/(site)/[locale]/[slug]/page.tsx
@@ -1,4 +1,4 @@
-// app/[locale]/[slug]/page.tsx
+// app/(site)/[locale]/[slug]/page.tsx
 import ContentAreaMapper from '@/components/content-area/mapper'
 import { optimizely } from '@/lib/optimizely/fetch'
 import {
@@ -15,17 +15,34 @@ export async function generateMetadata(props: {
 }): Promise<Metadata> {
   const { locale, slug = '' } = await props.params
   const locales = getValidLocale(locale)
+  const formattedSlug = `/${slug}`
   const { data, errors } = await optimizely.getPageByURL({
     locales: [locales],
-    slug: `/${slug}`,
+    slug: formattedSlug,
   })
 
-  if (errors || !data?.CMSPage?.items?.[0]) {
+  if (errors) {
     return {}
   }
 
-  const page = data.CMSPage.items[0]
+  const page = data?.CMSPage?.items?.[0]
   if (!page) {
+    const experienceData = await optimizely.GetVisualBuilderBySlug({
+      locales: [locales],
+      slug: formattedSlug,
+    })
+
+    const experience = experienceData.data?.SEOExperience?.items?.[0]
+
+    if (experience) {
+      return {
+        title: experience?.title,
+        description: experience?.shortDescription || '',
+        keywords: experience?.keywords ?? '',
+        alternates: generateAlternates(locale, formattedSlug),
+      }
+    }
+
     return {}
   }
 
@@ -33,32 +50,15 @@ export async function generateMetadata(props: {
     title: page.title,
     description: page.shortDescription || '',
     keywords: page.keywords ?? '',
-    alternates: generateAlternates(locale, '/'),
+    alternates: generateAlternates(locale, formattedSlug),
   }
 }
 
+
 export async function generateStaticParams() {
   try {
-    const pageTypes = ['CMSPage']
+    const pageTypes = ['CMSPage', 'SEOExperience']
     const pathsResp = await optimizely.AllPages({ pageType: pageTypes })
-    const paths = pathsResp.data?._Content?.items ?? []
-    const filterPaths = paths.filter(
-      (path) => path && path._metadata?.url?.default !== null
-    )
-    const uniquePaths = new Set<string>()
-    filterPaths.forEach((path) => {
-      const cleanPath = mapPathWithoutLocale(
-        path?._metadata?.url?.default ?? ''
-      )
-      uniquePaths.add(cleanPath)
-    })
-
-    return Array.from(uniquePaths).map((slug) => ({
-      slug,
-    }))
-  } catch (e) {
-    console.error(e)
-    return []
   }
 }
 
@@ -75,9 +75,25 @@ export default async function CmsPage(props: {
   })
 
   if (errors || !data?.CMSPage?.items?.[0]) {
+    const experienceData = await optimizely.GetVisualBuilderBySlug({
+      locales: [locales],
+      slug: formattedSlug,
+    })
+
+    const experience = experienceData.data?.SEOExperience?.items?.[0] as
+      | SafeVisualBuilderExperience
+      | undefined
+
+    if (experience) {
+      return (
+        <Suspense>
+          <VisualBuilderExperienceWrapper experience={experience} />
+        </Suspense>
+      )
+    }
+
     return notFound()
   }
-
   const page = data.CMSPage.items[0]
   const blocks = (page?.blocks ?? []).filter(
     (block) => block !== null && block !== undefined
